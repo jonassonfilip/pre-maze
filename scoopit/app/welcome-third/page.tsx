11da923f615627b49eeb9f91345fe4b36412feca
filjon2001 <filip.jonasson@hotmail.com>
2023-09-13 00:12:00 +0200

    Start setting up welcome pages

--- scoopit/app/welcome-third/page.tsx
+++ scoopit/app/welcome-third/page.tsx
@@ -1,13 +1,51 @@
 import { type } from "os";
 import React from "react";
 import Link from "next/link";
+import Image from "next/image";
+import scoopImage from "/public/scoop-purple.jpeg";
+import roundButton from "/public/Onboarding-button.png";
+import barThree from "/public/Bar3.png";
 
 export default function ThirdPage () {
     return (
-        <div>
-            <h1>Page 3</h1>
-            <p>Texttexttext</p>
-            <Link href="./login">Logga in</Link>
+        <main className="welcomePage">
+            <section>
+            <div className="welcomeHeader">
+            <Image
+          src={scoopImage}
+          alt="A scoop of ice cream"
+          width={390}
+          height={502}
+        />
+            </div>
+
+        <div className="welcomeFooter">
+            <h2>Besvärslös prenumerationshantering.</h2>
+            <p>Med Scoopit kommer du att samla in prenumerationer utan ansträngning. Spåra, hantera och ha kontroll över din ekonomi. Börja njuta av ett mer organiserat digitalt liv idag!</p>
         </div>
+        </section>
+
+        <section className="nextPage">
+        <div className="left">
+          <Image
+            src={barThree}
+            alt="Bar that shows where you are"
+            width={78}
+            height={4}
+          />
+        </div>
+        <div className="right">
+          <Link href="./login">
+            <Image
+              src={roundButton}
+              alt="Button to next page"
+              width={64}
+              height={64}
+            />
+          </Link>
+        </div>
+      </section>
+        
+        </main>
     )
 }
--- scoopit/app/welcome/page.tsx
+++ scoopit/app/welcome/page.tsx
@@ -2,34 +2,51 @@ import { type } from "os";
 import React from "react";
 import Link from "next/link";
 import Image from "next/image";
-import roundButton from "/public/Onboarding-button.png"
+import scoopImage from "/public/scoop-purple.jpeg";
+import roundButton from "/public/Onboarding-button.png";
+import barOne from "/public/Bar1.png";
 import RoundButton from "../components/RoundButton";
 
 
 export default function Welcome () {
     return (
         <main className="welcomePage">
+            <section>
             <div className="welcomeHeader">
-            
+            <Image
+          src={scoopImage}
+          alt="A scoop of ice cream"
+          width={390}
+          height={502}
+        />
             </div>
+
         <div className="welcomeFooter">
             <h2>Välkommen till Scoopit!</h2>
             <p>Säg adjö till prenumerationskaos. Vi har koll på att förenkla dina prenumerationer. Låt oss komma igång!</p>
         </div>
-        
-        <Link href="./welcome-second">
-        <div className="startPageButton">
-        <a>
-            
-            <Image
-          src={roundButton}
-          alt="Welcome Header Image"
-          width={64}
-          height={64} // Adjust the width and height as needed
-        />
-        </a>
+        </section>
+
+        <section className="nextPage">
+        <div className="left">
+          <Image
+            src={barOne}
+            alt="Bar that shows where you are"
+            width={78}
+            height={4}
+          />
         </div>
-        </Link>
+        <div className="right">
+          <Link href="./welcome-second">
+            <Image
+              src={roundButton}
+              alt="Button to next page"
+              width={64}
+              height={64}
+            />
+          </Link>
+        </div>
+      </section>
         
         </main>
     )

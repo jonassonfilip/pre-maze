ab7006ce498db32b9166022dd414f950fa970405
filjon2001 <filip.jonasson@hotmail.com>
2023-09-12 17:02:16 +0200

    Adding button

--- scoopit/app/welcome/page.tsx
+++ scoopit/app/welcome/page.tsx
@@ -2,6 +2,7 @@ import { type } from "os";
 import React from "react";
 import Link from "next/link";
 import Image from "next/image";
+import roundButton from "/public/Onboarding-button.png"
 import RoundButton from "../components/RoundButton";
 
 
@@ -9,26 +10,27 @@ export default function Welcome () {
     return (
         <main className="welcomePage">
             <div className="welcomeHeader">
-            <Image
-          src="/public/Onboarding-button.png"
-          alt="Welcome Header Image"
-          width={800}
-          height={400} // Adjust the width and height as needed
-        />
+            
             </div>
         <div className="welcomeFooter">
             <h2>Välkommen till Scoopit!</h2>
             <p>Säg adjö till prenumerationskaos. Vi har koll på att förenkla dina prenumerationer. Låt oss komma igång!</p>
         </div>
-        <div>
+        
         <Link href="./welcome-second">
-          
-            <div>
-                Next page
-            </div>
-          
-        </Link>
+        <div className="startPageButton">
+        <a>
+            
+            <Image
+          src={roundButton}
+          alt="Welcome Header Image"
+          width={64}
+          height={64} // Adjust the width and height as needed
+        />
+        </a>
         </div>
+        </Link>
+        
         </main>
     )
 }

69af4bc1220dcc494aeccdf28f9ece6476b5c2f9
Cellmon95 <lucas.arne@hotmail.com>
2023-09-13 17:12:31 +0200

    fixed some css, and started on home

--- scoopit/app/page.tsx
+++ scoopit/app/page.tsx
@@ -1,11 +1,8 @@
-import Image from 'next/image'
-import styles from './page.module.css'
-import Link from 'next/link'
 import Welcome from './welcome/page'
 
 export default function Home() {
   return (
-    <main className={styles.main}>
+    <main>
       <Welcome></Welcome>
     </main>
   )

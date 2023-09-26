a96384f6cbc0010f249609908d4d8eddd2932f3f
filjon2001 <filip.jonasson@hotmail.com>
2023-09-27 01:53:47 +0200

    Creating load-subscriptions page and removing some unnecessary pages etc.

---
+++ scoopit/app/components/LoadingBackground.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "./LoadingBackground.module.css";
+
+export default function Loadingbackground() {
+  return (
+    <main>
+      <div className={styles.background}></div>
+    </main>
+  );
+}
--- scoopit/app/start/page.tsx
+++ scoopit/app/start/page.tsx
@@ -1,25 +1,11 @@
-import { type } from 'os';
+import { type } from "os";
 import React from "react";
-import Link from "next/link";
-import Image from "next/image";
-import scoopImage from "/public/scoop-purple.jpeg";
-import scoopLogo from "/public/Scoopit-full-name.png";
-import pacman from "/public/pacman.png";
-import styles from "./page.module.css";
+import Loadingbackground from "../components/LoadingBackground";
 
-export function StartPage() {
-    return (
-        <main>
-            <section className={styles.startPage} style={{
-      backgroundImage: `url(${(scoopImage as any).src})`
-    }}>
-        {/* <Image
-      src={scoopLogo}
-      alt="The Scoopit logo"
-      width={102}
-      height={40}
-    /> */}
-        </section>
-        </main>
-    )
+export default function StartPage() {
+  return (
+    <main>
+      <Loadingbackground></Loadingbackground>
+    </main>
+  );
 }

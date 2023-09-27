f220c2ded0a527f3453ae6615340645d38469e84
filjon2001 <filip.jonasson@hotmail.com>
2023-09-27 11:42:52 +0200

    Adding logo to start page

--- scoopit/app/start/page.tsx
+++ scoopit/app/start/page.tsx
@@ -1,11 +1,15 @@
 import { type } from "os";
 import React from "react";
 import Loadingbackground from "../components/LoadingBackground";
+import styles from "./page.module.css";
 
 export default function StartPage() {
   return (
-    <main>
+    <main className={styles.page}>
       <Loadingbackground></Loadingbackground>
+      <div className={styles.logo}>
+        <h2>scoopit</h2>
+      </div>
     </main>
   );
 }
